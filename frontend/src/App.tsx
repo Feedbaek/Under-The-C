0f2a1bdfa08cb4865e3a2d001dad9d2c7185d340
OhHyukJun <kmh7277@naver.com>
2023-07-13 16:25:56 +0900

    Feat: 라우터 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Board from './Pages/Board/Board';
 import RegisterPage from './Pages/Auth/RegisterPage';
+import LoginPage from './Pages/Auth/LoginPage';
 import Main from './Pages/Main/Main';
 import UserPage from './Pages/User/UserPage';
+import Header from './Layout/Header';
+import Footer from './Layout/Footer';
 
-//<Route path="/" element={<Main/>}/>
 const App = () => {
   return (
     <>
+     <Header />
       {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Main/>}/>
-            <Route path="/board" element={<Board/>}/>
+            <Route path="/Board" element={<Board/>}/>
             <Route path="/UserPage" element={<UserPage/>}/>
             <Route path="/RegisterPage" element={<RegisterPage/>}/>
+            <Route path="/LoginPage" element={<LoginPage/>}/>
           </Routes>
         </BrowserRouter>
       }
+      <Footer />
     </>
   )
 }
